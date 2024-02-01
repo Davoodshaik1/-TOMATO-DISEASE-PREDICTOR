c420032393825306e765ce6cbc271ccbd93d092c
Davoodshaik1 <123303250+Davoodshaik1@users.noreply.github.com>
2024-02-01 21:27:07 +0530

    Initial commit

---
+++ gk.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.preprocessing import image
+
+# Load the trained model architecture
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(8, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(units=128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=10, activation='softmax'))
+
+# Load the trained model weights
+model.load_weights('keras_potato_trained_model_weights.h5')
+
+# Define the class labels
+class_labels = ['Bacterial_spot', 'Early_blight', 'Healthy', 'Late_blight', 'Leaf_Mold', 'Septoria_leaf_spot', 'Spider_mites Two-spotted_spider_mite', 'Target_Spot', 'Tomato_mosaic_virus', 'Tomato_Yellow_Leaf_Curl_Virus']
+
+# Function to preprocess the input image
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(128, 128))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+# Function to predict the disease based on class label
+def predict_disease(image_path):
+    processed_image = preprocess_image(image_path)
+    predictions = model.predict(processed_image)
+    predicted_class = np.argmax(predictions, axis=1)[0]
+    prediction_label = class_labels[predicted_class]
+    return prediction_label, predictions[0]
+
+# Streamlit app
+st.title("Tomato Disease Predictor")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    image_path = 'temp.jpg'  # Save the uploaded image as 'temp.jpg'
+    with open(image_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    prediction, probabilities = predict_disease(image_path)
+    st.write("Predicted Disease:", prediction)
+    st.write("Probabilities:", probabilities)
